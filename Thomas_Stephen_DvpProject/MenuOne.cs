f767ed816223b3ce20ef00c001dec9f08b3f5192
Stephen <thomasstephen91@gmai.com>
2018-08-29 19:56:59 -0500

    added menu list
    Added menu list

---
+++ Thomas_Stephen_DvpProject/MenuOne.cs
@@ -0,0 +1,125 @@
+﻿using System;
+namespace Thomas_Stephen_DvpProject
+{
+    public class MenuOne
+    {
+        //Menu 
+        //Swap Name
+        //Backwords;
+        //Age Coverter
+        //Temp Converter
+
+
+       
+        string mSwapName;
+        string mBackwards;
+        string mAgeConvert;
+        string mTempConvert;
+        string mBigBlueFish;
+        string mExit;
+
+        public MenuOne(string _swapName, string _backwords, string _ageConvert, string _tempConvert, string _bigBlueFish,string _exit)
+        {
+            mSwapName = _swapName;
+            mBackwards = _backwords;
+            mAgeConvert = _ageConvert;
+            mTempConvert = _tempConvert;
+            mBigBlueFish = _bigBlueFish;
+            mExit = _exit;
+
+        }
+
+        public string getSwapName()
+        {
+            return mSwapName;
+        }
+
+
+        public void setSwapName(string _swapName)
+        {
+
+            mSwapName = _swapName;
+
+
+
+        }
+
+
+        public string getBackwards()
+        {
+            return mBackwards;
+        }
+
+
+        public void setBackwards(string _backwards)
+        {
+
+            mBackwards = _backwards;
+
+
+
+        }
+        public string getAgeConvet()
+        {
+            return mAgeConvert;
+        }
+
+
+        public void setAgeconvert(string _ageConvert)
+        {
+
+            mAgeConvert = _ageConvert;
+
+
+
+        }
+        public string getTempConvert()
+        {
+            return mTempConvert;
+        }
+
+
+        public void setTempConvert(string _tempConvert)
+        {
+
+            mTempConvert = _tempConvert;
+
+
+
+        }
+        public string getBigBlueFish()
+        {
+            return mBigBlueFish;
+        }
+
+
+        public void setBigBlueFish(string _bigBlueFish)
+        {
+
+            mBigBlueFish = _bigBlueFish;
+
+
+
+        }
+        public string getExit()
+        {
+            return mExit;
+        }
+        public void setExit(string _exit)
+        {
+            mExit = _exit;
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Thomas_Stephen_DvpProject/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Thomas_Stephen_DvpProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //CAtch number va[lue
+
+            //call Menu one function
+            MenuOne codeChallenge = new MenuOne("[1] Swap Name","\r\n[2] Backwords","\r\n[3] Age Convert","\r\n[4] Temp Convert","\r\n[5] Big Blue Fish","[0] Exit");
+
+
+            Console.WriteLine("Coding Challenge Menu:\r\nPlease enter the number for the challenge you...");
+           
+            //Console getters and setters
+            Console.WriteLine(codeChallenge.getSwapName()+codeChallenge.getBackwards()+codeChallenge.getAgeConvet()+codeChallenge.getTempConvert()+codeChallenge.getBigBlueFish());
+
+            Console.WriteLine("\r\n"+codeChallenge.getExit());
+
+
+            Console.WriteLine("\r\n =======================");
+            Console.Write("Make your selection: ");
+            string userEnter = Console.ReadLine();
+
+            int numSwap;
+          
+            int.TryParse(userEnter, out numSwap);
+
+
+            
+
+
+
+
+
+
+           
+
+            
+            
+
+
+
+            
+            
+            
+             
+
+
+
+        }
+    }
+}
